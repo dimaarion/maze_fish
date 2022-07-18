bee6da0f6689a5fbbc111173d4085616194e436b
dimaarion <dimaarion@gmail.com>
2022-07-18 06:12:27 +0300

    dimaarion/maze_fish.git

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Loop, Stage, World, Body, Sprite } from 'react-game-kit/native';
+import { StyleSheet, Text, View ,Image} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>wefwef</Text>
+     
+<Loop>
+  <Stage>
+    <World>
+      <Sprite src = {"https://druzhniy-center.ru/wp-content/uploads/8/f/9/8f970e878337c2170713b4f20eafb065.jpeg"}  offset = {[0,0]} scale ={100}/>
+    </World>
+  </Stage>
+</Loop>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
